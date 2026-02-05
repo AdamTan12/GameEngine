fa475b598f6e8169a72667a93c49c5cf49b20c69
AdamTan12 <adamtan443@gmail.com>
2026-02-05 05:30:56 -0800

    added scale and rotation to transform ui in the editor

--- editor_imgui/Details/Details.cpp
+++ editor_imgui/Details/Details.cpp
@@ -1,4 +1,5 @@
 #include "Details.h"
+#include <iostream>
 // forward declarations
 void addMeshRenderer(GameObject* currGameObject);
 
@@ -48,6 +49,7 @@ void Details::transform(Transform* transform) {
     if (transform == nullptr)
         return;
     if (!ImGui::CollapsingHeader("Transform")) {
+        #pragma region pos
         ImGui::Text("Position");
         ImGui::Text("x");
         ImGui::SameLine();
@@ -68,5 +70,52 @@ void Details::transform(Transform* transform) {
         ImGui::PushItemWidth(75);
         ImGui::InputFloat("##z", &transform->position.z);
         ImGui::PopItemWidth();
+        #pragma endregion
+        #pragma region scale
+        ImGui::Text("Scale");
+        ImGui::Text("x");
+        ImGui::SameLine();
+        ImGui::PushItemWidth(75);
+        ImGui::InputFloat("##xScale", &transform->scale.x);
+        ImGui::PopItemWidth();
+        
+        ImGui::SameLine();
+        ImGui::Text("y");
+        ImGui::SameLine();
+        ImGui::PushItemWidth(75);
+        ImGui::InputFloat("##yScale", &transform->scale.y);
+        ImGui::PopItemWidth();
+
+        ImGui::SameLine();
+        ImGui::Text("z");
+        ImGui::SameLine();
+        ImGui::PushItemWidth(75);
+        ImGui::InputFloat("##zScale", &transform->scale.z);
+        ImGui::PopItemWidth();
+        #pragma endregion
+        #pragma region rotation
+        glm::vec3 euler = glm::degrees(glm::eulerAngles(transform->rotation));
+        ImGui::Text("Rotation");
+        ImGui::Text("x");
+        ImGui::SameLine();
+        ImGui::PushItemWidth(75);
+        ImGui::InputFloat("##xRot", &euler.x);
+        ImGui::PopItemWidth();
+        
+        ImGui::SameLine();
+        ImGui::Text("y");
+        ImGui::SameLine();
+        ImGui::PushItemWidth(75);
+        ImGui::InputFloat("##yRot", &euler.y);
+        ImGui::PopItemWidth();
+
+        ImGui::SameLine();
+        ImGui::Text("z");
+        ImGui::SameLine();
+        ImGui::PushItemWidth(75);
+        ImGui::InputFloat("##zRot", &euler.z);
+        ImGui::PopItemWidth();
+        transform->rotation = glm::quat(glm::radians(euler));
+        #pragma endregion
     }
 }
